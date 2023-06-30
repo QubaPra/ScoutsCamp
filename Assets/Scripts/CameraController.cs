c156bfe4daf896235b13bd403d92f26fd0c83180
QubaPra <116064097+QubaPra@users.noreply.github.com>
2023-06-30 15:47:23 +0200

    BIG CHANGE 2D -> 3D URP

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,9 +3,15 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     private Vector3 touchStart;
-    public float zoomOutMin = 4;
+    public float zoomOutMin = 5;
     public float zoomOutMax = 12;
-    public float moveSpeed = 1;
+    public float moveSpeed = 5;
+    private float originalYPosition;
+
+    private void Start()
+    {
+        originalYPosition = transform.position.y; // Store the original y position
+    }
 
     void Update()
     {
@@ -31,6 +37,7 @@ public class CameraController : MonoBehaviour
         else if (Input.GetMouseButton(0))
         {
             Vector3 direction = touchStart - Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            direction.y = 0; // Ignore the y component of the direction
             Camera.main.transform.position += direction * moveSpeed * Time.deltaTime;
         }
 
@@ -40,5 +47,8 @@ public class CameraController : MonoBehaviour
     void Zoom(float increment)
     {
         Camera.main.orthographicSize = Mathf.Clamp(Camera.main.orthographicSize - increment, zoomOutMin, zoomOutMax);
+        Vector3 newPosition = Camera.main.transform.position;
+        newPosition.y = originalYPosition; // Set the y position to the original value
+        Camera.main.transform.position = newPosition;
     }
 }
